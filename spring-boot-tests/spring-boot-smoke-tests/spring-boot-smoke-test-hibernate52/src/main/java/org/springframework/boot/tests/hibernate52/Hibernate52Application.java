0d1f8bb71682dbca2d21a5068556bb7774e520ff
chen zeng <chenzeng@yuanfudao.com>
2021-09-01 18:06:20 +0800

    First init.

--- spring-boot-tests/spring-boot-smoke-tests/spring-boot-smoke-test-hibernate52/src/main/java/org/springframework/boot/tests/hibernate52/Hibernate52Application.java
+++ spring-boot-tests/spring-boot-smoke-tests/spring-boot-smoke-test-hibernate52/src/main/java/org/springframework/boot/tests/hibernate52/Hibernate52Application.java
@@ -23,6 +23,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Hibernate52Application {
 
 	public static void main(String[] args) {
+		// Test
 		SpringApplication.run(Hibernate52Application.class, args);
 	}
 
